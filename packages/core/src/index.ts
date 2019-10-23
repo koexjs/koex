ebbc411bc4cf7cb10ab0e606170313777f54b037
whatwewant <tobewhatwewant@gmail.com>
2019-10-23 15:07:04 +0800

    :sparkles:(core): support all method

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,6 +3,10 @@ import { Middleware } from 'koa-compose';
 
 import { router } from './router';
 
+export {
+  Middleware,
+};
+
 export interface Context extends Koa.Context {
 
 }
@@ -18,6 +22,10 @@ export class Koex extends Koa {
 
   public get router() {
     return router;
+  }
+
+  public all(path: string, ...middlewares: Middleware<Context>[]) {
+    this.use(this.router.all(path, ...middlewares));
   }
 
   public get(path: string, ...middlewares: Middleware<Context>[]) {
