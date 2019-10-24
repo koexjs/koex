c96b60e06e2511d56c5df95998013ea141531efe
whatwewant <tobewhatwewant@gmail.com>
2019-10-24 22:29:43 +0800

    :sparkles:(@koex/passport): export IStrategyProfile

--- packages/passport/src/index.ts
+++ packages/passport/src/index.ts
@@ -1,11 +1,13 @@
 import { passport, Passport, Stage, IPassport } from './passport';
 import {
+  IStrategyProfile,
   IGetUserByStrategyProfile,
   Strategy,
 } from './strategy';
 
 export {
   IPassport,
+  IStrategyProfile,
   IGetUserByStrategyProfile,
   Stage,
   Strategy,
