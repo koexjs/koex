48ee170582e8d95386b6aed5ec6c50695923862e
whatwewant <tobewhatwewant@gmail.com>
2019-10-24 22:30:31 +0800

    :sparkles:(@koex/passport-oauth2): refactor passport-oauth2, authentication and callback flow freeze
    * Oauth Passport Strategy
      * which defined the authenticate(@S1) and callback flow(@S2),
      * what you need do is to realize the following three methods,
      * which will used in authenticate and callback flow.
        * @Method getAuthorizeUrl(url, data): URL (@S1)
        * @Method getAccessToken(url, data): Token (@S2)
        * @Method getAccessUser(url, data): User (@S3)

--- packages/passport-oauth2/src/index.ts
+++ packages/passport-oauth2/src/index.ts
@@ -1,9 +1,17 @@
 export { IGetUserByStrategyProfile } from '@koex/passport';
-import { OauthStrategy, IOauthStrategyOptions } from './strategy';
+import {
+  OauthStrategy, IOauthStrategyOptions,
+  IGetAuthorizeUrlData,
+  IGetAccessTokenData,
+  Profile,
+} from './strategy';
 
 export {
   OauthStrategy,
   IOauthStrategyOptions,
+  IGetAuthorizeUrlData,
+  IGetAccessTokenData,
+  Profile,
 }
 
 export const Strategy = OauthStrategy;
--- packages/passport-oauth2/src/strategy.ts
+++ packages/passport-oauth2/src/strategy.ts
@@ -1,7 +1,13 @@
+import { Context } from '@koex/core';
+
 import fetch from 'node-fetch';
 import * as qs from '@zcorky/query-string';
 
-import { Strategy, IGetUserByStrategyProfile } from '@koex/passport';
+import {
+  Strategy,
+  IStrategyProfile,
+  IGetUserByStrategyProfile,
+} from '@koex/passport';
 
 export interface IOauthStrategyOptions {
   /**
@@ -23,6 +29,11 @@ export interface IOauthStrategyOptions {
    * Scope / Permission
    */
   scope?: string;
+
+  /**
+   * State / Security
+   */
+  state?: string;
 
   /**
    * Response Type
@@ -49,100 +60,184 @@ export interface IOauthStrategyOptions {
   /**
    * User Info Url
    */
-  user_info_url: string;
+  user_profile_url: string;
 }
 
+export interface IGetAuthorizeUrlData {
+  /**
+   * Client ID
+   */
+  client_id: string;
+
+  /**
+   * Redirect URI (callback url)
+   */
+  redirect_uri: string;
+
+  /**
+   * Response Type
+   *  default: code
+   */
+  response_type?: string;
+
+  /**
+   * Scope / Permission
+   */
+  scope?: string;
+
+  /**
+   * State / Security
+   */
+  state?: string;
+}
+
+export interface IGetAccessTokenData {
+  /**
+   * Client ID
+   */
+  client_id: string;
+
+  /**
+   * Client ID
+   */
+  client_secret: string;
+
+  /**
+   * Redirect URI (callback url)
+   */
+  redirect_uri: string;
+
+  /**
+   * Grant Type
+   *  default: authorization_code
+   */
+  grant_type?: string;
+
+  /**
+   * Scope / Permission
+   */
+  scope?: string;
+
+  /**
+   * Authorization Code
+   */
+  code: string;
+}
+
+/**
+ * Access Token
+ */
+export type Token = any;
+
+/**
+ * User Profile
+ */
+export type Profile = IStrategyProfile;
+
+/**
+ * Oauth Passport Strategy
+ *  which defined the authenticate(@S1) and callback flow(@S2),
+ *  what you need do is to realize the following three methods,
+ *  which will used in authenticate and callback flow.
+ *    @Method getAuthorizeUrl(url, data): URL (@S1)
+ *    @Method getAccessToken(url, data): Token (@S2)
+ *    @Method getAccessUser(url, data): User (@S3)
+ */
 export abstract class OauthStrategy extends Strategy {
-  constructor(private readonly oauthStrategyOptions: IOauthStrategyOptions, public readonly getUserByStrategyProfile: IGetUserByStrategyProfile) {
+  constructor(protected readonly oauthStrategyOptions: IOauthStrategyOptions, public readonly getUserByStrategyProfile: IGetUserByStrategyProfile) {
     super(getUserByStrategyProfile);
   }
 
-  protected async getAuthorizeUrl() {
+  /**
+   * Get Authorize Url
+   * 
+   * @param authorize_base_url base authorize url
+   * @param data the full authorize url required data
+   * @returns the full authorize url, mostly used to redirect
+   */
+  protected abstract getAuthorizeUrl(authorize_base_url: string, data: IGetAuthorizeUrlData): Promise<string>
+
+  /**
+   * Get Access Token
+   * 
+   * @param token_url base token url
+   * @param data the data required to get access token
+   */
+  protected abstract getAccessToken(token_url: string, data: IGetAccessTokenData): Promise<Token>;
+  
+  /**
+   * Get Access User
+   * 
+   * @param user_profile_url base user info url, here, just wants to openid, or user
+   * @param access_token the getAccessToken() return data
+   */
+  protected abstract getAccessUser(user_profile_url: string, access_token: Token): Promise<Profile>;
+
+  /**
+   * @S1 Authenticate Flow 
+   * 
+   * @param ctx context
+   * @returns redirect to authorization url
+   */
+  public async authenticate(ctx: Context) {
     const {
       authorize_url,
       client_id,
       response_type,
       redirect_uri,
       scope,
+      state,
     } = this.oauthStrategyOptions;
-
+    
     const data = {
       client_id,
-      response_type,
       redirect_uri,
+      response_type,
       scope,
+      state,
     };
 
-    const url = `${authorize_url}?${qs.stringify(data)}`;
-
-    return url;
+    const auth_server_url = await this.getAuthorizeUrl(authorize_url, data);
+    ctx.redirect(auth_server_url);
   }
 
-  protected async getAccessToken<Token = any>(code: string): Promise<Token> {
+  /**
+   * @S2 Callback Flow 
+   * 
+   * @param ctx context
+   * @returns user profile
+   */
+  public async callback(ctx: Context): Promise<Profile> {
     const {
       token_url,
+      user_profile_url,
       client_id,
       client_secret,
-      grant_type,
       redirect_uri,
+      grant_type,
       scope,
     } = this.oauthStrategyOptions;
 
-    const data = {
+    const code = ctx.query.code;
+
+    const accessTokenData = {
       client_id,
       client_secret,
-      grant_type,
       redirect_uri,
+      grant_type,
       scope,
       code,
     };
 
-    const method = 'POST';
+    const token = await this.getAccessToken(token_url, accessTokenData);
 
-    const headers = {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-    };
+    const profile = await this.getAccessUser(user_profile_url, token);
 
-    const response = await fetch(token_url, {
-      method,
-      headers,
-      body: JSON.stringify(data),
-    });
-
-    if (!response.ok) {
-      const status = response.status;
-      const message = await response.text();
-      throw new Error(`[oauth.token][${status}] ${message}`);
-    }
-
-    return response.json();
+    return profile;
   }
 
-  protected async getAccessUser<Profile = any>(access_token: string): Promise<Profile> {
-    const {
-      user_info_url,
-    } = this.oauthStrategyOptions;
-
-    const method = 'GET';
-
-    const headers = {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-      'Authorization': `Bearer ${access_token}`,
-    };
-
-    const response = await fetch(user_info_url, {
-      method,
-      headers,
-    });
-
-    if (!response.ok) {
-      const status = response.status;
-      const message = await response.text();
-      throw new Error(`[oauth.user][${status}] ${message}`);
-    }
-
-    return response.json();
-  }
+  protected readonly utils = {
+    fetch,
+    qs,
+  };
 }
