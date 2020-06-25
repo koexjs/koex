79781414f8972cbf4e0353c35cdccb0f62f57a75
whatwewant <tobewhatwewant@gmail.com>
2020-06-25 13:40:18 +0800

    feat(@koex/core): add appCache(app.cache) and contextCache(ctx.cache)

--- packages/core/src/helpers.ts
+++ packages/core/src/helpers.ts
@@ -1,26 +1,34 @@
 import { Koex } from './index';
-import { Context } from './types';
 
 import { Logger } from '@zodash/logger';
+import { lru as LRU } from '@zcorky/lru';
 
 declare module '.' {
   export interface Koex {
     logger: Logger;
+    cache: LRU<string, any>;
   }
 
   export interface Context {
     logger: Logger;
+    cache: LRU<string, any>;
   }
 }
 
 export function createHelpers(app: Koex) {
+  // logger
   const appLogger = new Logger('app');
   const contextLogger = new Logger('context');
+  // cache
+  const appCache = new LRU<string, any>();
+  const contextCache = new LRU<string, any>();
 
   app.logger = appLogger;
+  app.cache = appCache;
 
   app.use(async (ctx, next) => {
     ctx.logger = contextLogger;
+    ctx.cache = contextCache;
 
     return next();
   });
