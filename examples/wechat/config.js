c55d7762041a8ada4efdef67a95d92d84378c3c2
whatwewant <tobewhatwewant@gmail.com>
2020-07-25 11:02:35 +0800

    chore

--- examples/wechat/config.js
+++ examples/wechat/config.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 module.exports = {
-  appId: 'wx714203b77d9b2261',
-  appSecret: 'd7b97ac21a11df854b8145d97e53eeb5',
+  appId: process.env.APP_ID,
+  appSecret: process.env.APP_SECRET,
   accessTokenUrl: `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={appId}&secret={appSecret}`,
   jsapiTicketUrl: 'https://api.weixin.qq.com/cgi-bin/ticket/getticket?type=jsapi&access_token={accessToken}',
 };
